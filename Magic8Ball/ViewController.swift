ce43a158328725be1b0eb3a71b3ec0ad450cafc4
razeiv07 <rajeevxrma@gmail.com>
2024-07-06 23:20:20 -0400

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Rajeev Sharma on 2024-07-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
